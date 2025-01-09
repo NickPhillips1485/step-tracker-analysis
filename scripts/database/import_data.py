370f45df614c13f6805192e664040280a99d33b9
Nick Phillips <nickphillips8888@hotmail.co.uk>
2025-01-09 17:49:45 +0000

    Inserting the data from the csv file into the database

--- scripts/database/import_data.py
+++ scripts/database/import_data.py
@@ -10,4 +10,7 @@ data = pd.read_csv(r'C:\Users\nickp\Documents\step-tracker-analysis\source data.
 # Preview the data
 print(data.head())
 
+# Insert data into the table
+for index, row in data.iterrows():
+    cursor.execute('INSERT INTO steps (year, month, miles) VALUES (?, ?, ?)', (row['Year'], row['Month'], row['Miles']))
 
